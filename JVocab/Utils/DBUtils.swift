c80f05bc451683b31d48875efe028bd383f243ae
Nam Dinh Vu <namdinh@hiworld.com.vn>
2017-09-14 14:19:32 +0700

    refactor code

--- JVocab/Utils/DBUtils.swift
+++ JVocab/Utils/DBUtils.swift
@@ -14,10 +14,6 @@ class DBUtils {
     
     fileprivate let context = AppDelegate.shared().persistentContainer.viewContext
     
-    let higarana = ["あ","い","う","え","お","か","き","く","け","こ","さ","し","す","せ","そ","た","ち","つ","て","と","な","に","ぬ","ね","の","は","ひ","ふ","へ","ほ","ま","み","む","め","も","や","ゆ","よ","ら","り","る","れ","ろ","わ","ゐ","ゑ","を", "ん"]
-    let katakana = ["イ","ウ","エ","オ","カ","キ","ク","ケ","コ","サ","シ","ス","セ","ソ","タ","チ","ツ","テ","ト","ナ","ニ","ヌ","ネ","ノ","ハ","ヒ","フ","ヘ","ホ","マ","ミ","ム","メ","モ","ヤ","ユ","ヨ","ラ","リ","ル","レ","ロ","ワ","ヰ","ヱ","ヲ", "ン"]
-    
-    
     func fetchData() -> [Word] {
         var words = [Word]()
         do {
@@ -99,7 +95,7 @@ class DBUtils {
     }
     
     func getAllKana() -> [String] {
-        return higarana + katakana
+        return DBData.instance.higarana + DBData.instance.katakana
     }
     
     func getContext() -> NSManagedObjectContext {
